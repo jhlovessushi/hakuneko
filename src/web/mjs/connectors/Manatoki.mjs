2196252783f5b89a16f35350c6c5bc416870cc67
jhlovessushi <108144775+jhlovessushi@users.noreply.github.com>
2025-02-18 19:21:48 +0900

    Update Manatoki.mjs
    updated link of manatoki

--- src/web/mjs/connectors/Manatoki.mjs
+++ src/web/mjs/connectors/Manatoki.mjs
@@ -7,7 +7,7 @@ export default class Manatoki extends GnuBoard5BootstrapBasic2 {
         super.id = 'manatoki';
         super.label = 'Manatoki';
         this.tags = [ 'manga', 'webtoon', 'korean' ];
-        this.url = 'https://manatoki.net';
+        this.url = 'https://manatoki468.net';
 
         this.path = [ '/webtoon', '/comic' ];
         this.queryMangasPageCount = 'div.list-page ul.pagination li:last-child a';
